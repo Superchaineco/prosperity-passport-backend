3511b1696c675789093c6c20c8e6e7a8d6a66308
voyager-seb <seb@kolektivo.co>
2025-02-03 04:21:11 -0500

    feat(badges): cached service

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,24 +1,34 @@
 import { redis } from "../utils/cache";
 
 export class RedisService {
-    public async getCachedDataWithCallback<T>(key: string, fetchFunction: () => Promise<T>, ttl?: number): Promise<T> {
-        const cachedData = await redis.get(key);
-        if (cachedData) {
-            console.info(`Cache hit for key: ${key}`);
-            return JSON.parse(cachedData);
-        }
+    public async getCachedDataWithCallback<T>(key: string, fetchFunction: () => Promise<T>, ttl: number): Promise<T> {
+        try {
+            const cachedData = await redis.get(key);
+            if (cachedData) {
+                console.info(`Cache hit for key: ${key}`);
+                return JSON.parse(cachedData);
+            }
 
-        const data = await fetchFunction();
-        if (ttl) {
-            await redis.set(key, JSON.stringify(data), "EX", ttl);
-        } else {
-            await redis.set(key, JSON.stringify(data));
+            const data = await fetchFunction();
+
+            if (ttl > 0) {
+                await redis.set(key, JSON.stringify(data), "EX", ttl);
+            }
+            else {
+                await redis.set(key, JSON.stringify(data));
+            }
+            return data;
+        } catch (error) {
+            console.error('Error getting cached data', error);
+            throw error;
         }
-        return data;
     }
 
     public async setCachedData(key: string, data: any, ttl: number) {
-        await redis.set(key, JSON.stringify(data), "EX", ttl);
+        if (ttl)
+            await redis.set(key, JSON.stringify(data), "EX", ttl);
+        else
+            await redis.set(key, JSON.stringify(data));
     }
 
     public async getCachedData(key: string) {

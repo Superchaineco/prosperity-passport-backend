5d70a5470fd5beb45cd33f1ca4b1f7c7b5930b84
sebaxor <sebasor@gmail.com>
2025-03-26 14:54:57 -0500

    choring: wiring env vars

--- src/config/superChain/constants.ts
+++ src/config/superChain/constants.ts
@@ -1,39 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export enum ENVIRONMENTS {
-  development = "development",
-  production = "production",
-}
+// export enum ENVIRONMENTS {
+//   development = "development",
+//   production = "production",
+// }
 
-export const ENV = process.env.NODE_ENV || ENVIRONMENTS.development;
+// export const ENV =
+//   (process.env.NODE_ENV as ENVIRONMENTS) || ENVIRONMENTS.development;
 
 const config = {
-  development: {
-    SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS:
-      "0xbC24012488BFCFe44875a139299595879c43C2CD",
-    SUPER_CHAIN_ATTESTATION_SCHEMA:
-      "0x1492ec328ed4c8e25f2be8247a70779b42e89dae11b5625827831c95cd99b052",
-    EAS_CONTRACT_ADDRESS: "0x72E1d8ccf5299fb36fEfD8CC4394B8ef7e98Af92",
-    JSON_RPC_PROVIDER: process.env.JSON_RPC_PROVIDER,
-    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-    DOMAIN: "https://staging.account.superchain.eco",
-    REDIS: process.env.REDIS_PUBLIC_URL,
-    SAFE_ADDRESS: "0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
-  },
-  production: {
-    SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS:
-      "0x58f5805b5072C3Dd157805132714E1dF40E79c66",
-    SUPER_CHAIN_ATTESTATION_SCHEMA:
-      "0x1492ec328ed4c8e25f2be8247a70779b42e89dae11b5625827831c95cd99b052",
-    EAS_CONTRACT_ADDRESS: "0x72E1d8ccf5299fb36fEfD8CC4394B8ef7e98Af92",
-    JSON_RPC_PROVIDER: process.env.JSON_RPC_PROVIDER,
-    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-    DOMAIN: "http://localhost:3000",
-    REDIS: process.env.REDIS_URL,
-    SAFE_ADDRESS: "0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
-  },
-}[ENV];
+  //   development: {
+  //     SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS:
+  //       "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded",
+  //     SUPER_CHAIN_ATTESTATION_SCHEMA:
+  //       "0xb77b597c6b8404139df340a738e9252ff7e758564562d34694ebfd8270270865",
+  //     EAS_CONTRACT_ADDRESS: "0x4200000000000000000000000000000000000021",
+  //     JSON_RPC_PROVIDER: process.env.JSON_RPC_PROVIDER,
+  //     ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+  //     DOMAIN: process.env.DOMAIN,//"http://localhost:3000",
+  //     REDIS: process.env.REDIS_PUBLIC_URL,
+  //     SAFE_ADDRESS: "0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
+  //   },
+  //   production: {
+  SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS: process.env.SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS, //    "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded",
+  SUPER_CHAIN_ATTESTATION_SCHEMA: process.env.SUPER_CHAIN_ATTESTATION_SCHEMA,//      "0xb77b597c6b8404139df340a738e9252ff7e758564562d34694ebfd8270270865",
+  EAS_CONTRACT_ADDRESS: process.env.EAS_CONTRACT_ADDRESS,// "0x4200000000000000000000000000000000000021",
+  JSON_RPC_PROVIDER: process.env.JSON_RPC_PROVIDER,
+  ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+  DOMAIN: process.env.DOMAIN,// "https://account.superchain.eco",
+  REDIS: process.env.REDIS_URL,
+  SAFE_ADDRESS: process.env.SAFE_ADDRESS//"0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
+}//,
+// }[ENV];
 
 import SuperChainModuleABI from "./abi/SuperChainModule.json";
 import SunnyAirdropABI from "./abi/SunnyAirdrop.json";
@@ -62,6 +61,5 @@ export const WC_PROJECT_ID = process.env.WC_PROJECT_ID!;
 export const DOMAIN = config.DOMAIN;
 export const SESSION_SECRET = process.env.SESSION_SECRET!;
 export const DUNE_API_KEY = process.env.DUNE_API_KEY!;
-export const SAFE_ADDRESS =  config.SAFE_ADDRESS
-export const PIMLICO_API_KEY =  process.env.PIMLICO_API_KEY
-export const SUBGRAPH_URI = process.env.SUBGRAPH_URI!;
+export const SAFE_ADDRESS = config.SAFE_ADDRESS
+export const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY

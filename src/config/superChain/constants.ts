495e83ed63c0211006800c3a4c36c3d4884f44c9
anotherdev <joseluismanco37@gmail.com>
2025-03-26 15:30:29 -0500

    fix: environment

--- src/config/superChain/constants.ts
+++ src/config/superChain/constants.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 export enum ENVIRONMENTS {
-  development = "development",
-  production = "production",
+  development = 'development',
+  production = 'production',
 }
 
 export const ENV =
@@ -23,19 +23,20 @@ const config = {
   //     SAFE_ADDRESS: "0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
   //   },
   //   production: {
-  SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS: process.env.SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS, //    "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded",
-  SUPER_CHAIN_ATTESTATION_SCHEMA: process.env.SUPER_CHAIN_ATTESTATION_SCHEMA,//      "0xb77b597c6b8404139df340a738e9252ff7e758564562d34694ebfd8270270865",
-  EAS_CONTRACT_ADDRESS: process.env.EAS_CONTRACT_ADDRESS,// "0x4200000000000000000000000000000000000021",
+  SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS:
+    process.env.SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS, //    "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded",
+  SUPER_CHAIN_ATTESTATION_SCHEMA: process.env.SUPER_CHAIN_ATTESTATION_SCHEMA, //      "0xb77b597c6b8404139df340a738e9252ff7e758564562d34694ebfd8270270865",
+  EAS_CONTRACT_ADDRESS: process.env.EAS_CONTRACT_ADDRESS, // "0x4200000000000000000000000000000000000021",
   JSON_RPC_PROVIDER: process.env.JSON_RPC_PROVIDER,
   ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-  DOMAIN: process.env.DOMAIN,// "https://account.superchain.eco",
-  REDIS: process.env.REDIS_URL,
-  SAFE_ADDRESS: process.env.SAFE_ADDRESS//"0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
-}//,
+  DOMAIN: process.env.DOMAIN, // "https://account.superchain.eco",
+  REDIS: process.env.REDIS_PUBLIC_URL,
+  SAFE_ADDRESS: process.env.SAFE_ADDRESS, //"0x54efe9c4d7D91E2690f257A7855AAD4b21e20745"
+}; //,
 // }[ENV];
 
-import SuperChainModuleABI from "./abi/SuperChainModule.json";
-import SunnyAirdropABI from "./abi/SunnyAirdrop.json";
+import SuperChainModuleABI from './abi/SuperChainModule.json';
+import SunnyAirdropABI from './abi/SunnyAirdrop.json';
 export const SUPER_CHAIN_MODULE_ABI = SuperChainModuleABI;
 export const SUNNY_AIRDROP_ABI = SunnyAirdropABI;
 export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS =
@@ -61,5 +62,6 @@ export const WC_PROJECT_ID = process.env.WC_PROJECT_ID!;
 export const DOMAIN = config.DOMAIN;
 export const SESSION_SECRET = process.env.SESSION_SECRET!;
 export const DUNE_API_KEY = process.env.DUNE_API_KEY!;
-export const SAFE_ADDRESS = config.SAFE_ADDRESS
-export const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY
+export const SAFE_ADDRESS = config.SAFE_ADDRESS;
+export const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY;
+export const SUBGRAPH_URI = process.env.SUBGRAPH_URI!;

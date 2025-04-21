d9eef11a0d728fc8dc3a9b52a0df4e14937e7566
anotherdev <joseluismanco37@gmail.com>
2025-04-21 14:51:24 -0500

    feat: new name for L1 badge

--- src/services/badges/strategies/context.ts
+++ src/services/badges/strategies/context.ts
@@ -23,51 +23,51 @@ import { TalentScoreStrategy } from './talentScoreStrategy';
 
 
 export class BadgeStrategyContext {
-    static getBadgeStrategy(badgeName: string): BadgeStrategy {
-        switch (badgeName.trim()) {
+  static getBadgeStrategy(badgeName: string): BadgeStrategy {
+    switch (badgeName.trim()) {
 
-            case "Celo Genesis":
-                return new CeloGenesisStrategy()
-            case "L1 Transactions":
-                return new CeloTransactionsStrategy()
-            case "CEL2 Transactions":
-                return new Celo2TransactionsStrategy()
-            case "Giveth Donor":
-                return new GivethDonationsStrategy()
-            case "Gitcoin Donor":
-                return new GitcoinDonationsStrategy()
-            case "Talent Protocol Score":
-                return new TalentScoreStrategy()
-            case "Celo Voter":
-                return new CeloVotesStrategy()
-            case "CeloPG Steward":
-                return new CeloStewardsStrategy()
-            case "Regional DAO Lead":
-                return new CeloRegionalLeadStrategy()
-            case "USD GLO tiers":
-                return new GlodollarStrategy()
-            case "ReFi DAO Contributor":
-                return new ReFiDaoMemberStrategy()
-            case "Community Guild":
-                return new CeloCommunityGuildStrategy()
-            case "Governance Guardian":
-                return new CeloGovernanceGuardianStrategy()
-            case "Proof of Ship":
-                return new ProofOfShipStrategy()
-            case "Eco Credit Retirement":
-                return new EcoCreditsStrategy()
-            case "Self verification":
-                return new SelfVerificationStrategy()
-            case "GreenPill Member":
-                return new GreenPillMemberStrategy()
-            case "Celo Citizen":
-                return new CeloCitizenStrategy()
-            case "Celo Event POAPs":
-                return new CeloEventsPoapStrategy()
-            default:
-                throw new Error(`Badge strategy ${badgeName} not found`);
-        }
+      case "Celo Genesis":
+        return new CeloGenesisStrategy()
+      case "CEL1 Transactions":
+        return new CeloTransactionsStrategy()
+      case "CEL2 Transactions":
+        return new Celo2TransactionsStrategy()
+      case "Giveth Donor":
+        return new GivethDonationsStrategy()
+      case "Gitcoin Donor":
+        return new GitcoinDonationsStrategy()
+      case "Talent Protocol Score":
+        return new TalentScoreStrategy()
+      case "Celo Voter":
+        return new CeloVotesStrategy()
+      case "CeloPG Steward":
+        return new CeloStewardsStrategy()
+      case "Regional DAO Lead":
+        return new CeloRegionalLeadStrategy()
+      case "USD GLO tiers":
+        return new GlodollarStrategy()
+      case "ReFi DAO Contributor":
+        return new ReFiDaoMemberStrategy()
+      case "Community Guild":
+        return new CeloCommunityGuildStrategy()
+      case "Governance Guardian":
+        return new CeloGovernanceGuardianStrategy()
+      case "Proof of Ship":
+        return new ProofOfShipStrategy()
+      case "Eco Credit Retirement":
+        return new EcoCreditsStrategy()
+      case "Self verification":
+        return new SelfVerificationStrategy()
+      case "GreenPill Member":
+        return new GreenPillMemberStrategy()
+      case "Celo Citizen":
+        return new CeloCitizenStrategy()
+      case "Celo Event POAPs":
+        return new CeloEventsPoapStrategy()
+      default:
+        throw new Error(`Badge strategy ${badgeName} not found`);
     }
+  }
 
 
 }

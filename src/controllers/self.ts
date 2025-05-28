2f6ccbe35006d35fcee98cea1dd70915e87476d4
sebaxor <sebasor@gmail.com>
2025-05-28 15:02:44 -0500

    feat(self): remove mock validations

--- src/controllers/self.ts
+++ src/controllers/self.ts
@@ -25,7 +25,7 @@ export default async function selfVerify(req: Request, res: Response) {
                 'prosperity',
                 'https://prosperity-passport-backend-production.up.railway.app/api/self/verify',
                 'hex',
-                true
+                false
             );
 
             const result = await selfBackendVerifier.verify(proof, publicSignals);

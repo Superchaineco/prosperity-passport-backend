13282d44d9399bdc04e732592e11533114966da7
sebaxor <sebasor@gmail.com>
2025-05-28 13:57:05 -0500

    feat(self): enable self claiming

--- src/services/badges/strategies/selfVerificationStrategy.ts
+++ src/services/badges/strategies/selfVerificationStrategy.ts
@@ -10,9 +10,9 @@ export class SelfVerificationStrategy extends BaseBadgeStrategy {
     }
     async getValue(eoas: string[]): Promise<boolean> {
 
-        
-        //const cache_key = `self_id:${this.account.toUpperCase()}`
-        const cache_key = `self_id:${eoas.join(",")}`
+
+        const cache_key = `self_id:${this.account.toUpperCase()}`
+        //const cache_key = `self_id:${eoas.join(",")}`
 
         const selfData = redisService.getCachedData(cache_key)
 

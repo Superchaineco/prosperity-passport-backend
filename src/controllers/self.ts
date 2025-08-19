8b770678281af08081fd8f5337c9c20146d438a5
anotherdev <joseluismanco37@gmail.com>
2025-08-19 18:51:15 -0500

    feat: upgrade self package

--- src/controllers/self.ts
+++ src/controllers/self.ts
@@ -1,126 +1,123 @@
 import { Request, Response } from 'express';
-import { getUserIdentifier } from '@selfxyz/core';
 import { redisService } from '@/services/redis.service';
 import { SelfService } from '@/services/badges/self.service';
+import { castToUserIdentifier } from '@selfxyz/common/utils/circuits/uuid';
 import { Console } from 'console';
 
-
-
 export default async function selfVerify(req: Request, res: Response) {
-    if (req.method === 'POST') {
-        const { proof, publicSignals } = req.body;
+  if (req.method === 'POST') {
+    const { attestationId, proof, publicSignals, userContextData } = req.body;
 
-        if (!proof || !publicSignals) {
-            return res
-                .status(400)
-                .json({ message: 'Proof and publicSignals are required' });
-        }
-        const userId = await getUserIdentifier(publicSignals);
-        console.log('Extracted userId:', userId);
-
-        const selfService = new SelfService()
-        try {
-            const result = await selfService.selfVerify(userId, proof, publicSignals);
-            console.log('Verification result:', result);
-
-            if (result.isValid) {
-
-                return res.status(200).json({
-                    status: 'success',
-                    result: true,
-                    credentialSubject: result.credentialSubject,
-                });
-            } else {
-
-                return res.status(500).json({
-                    status: 'error',
-                    result: false,
-                    message: 'Verification failed',
-                    details: result.details,
-                });
-            }
-        } catch (error) {
-            console.error('Error verifying proof:', error);
-            return res.status(500).json({
-                status: 'error',
-                result: false,
-                message: error instanceof Error ? error.message : 'Unknown error',
-            });
-        }
-
-    } else {
-        return res.status(405).json({ message: 'Method not allowed' });
+    if (!proof || !publicSignals) {
+      return res
+        .status(400)
+        .json({ message: 'Proof and publicSignals are required' });
     }
+
+    const selfService = new SelfService();
+    const userIdentifier = castToUserIdentifier(
+      BigInt('0x' + userContextData.slice(64, 128)),
+      'hex'
+    );
+
+    try {
+      const result = await selfService.selfVerify(
+        userIdentifier,
+        proof,
+        publicSignals,
+        attestationId,
+        userContextData
+      );
+      console.log('Verification result:', result);
+
+      if (result.isValid) {
+        return res.status(200).json({
+          status: 'success',
+          result: true,
+          credentialSubject: result.credentialSubject,
+        });
+      } else {
+        return res.status(500).json({
+          status: 'error',
+          result: false,
+          message: 'Verification failed',
+          details: result.details,
+        });
+      }
+    } catch (error) {
+      console.error('Error verifying proof:', error);
+      return res.status(500).json({
+        status: 'error',
+        result: false,
+        message: error instanceof Error ? error.message : 'Unknown error',
+      });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 }
 
 export async function selfCheck(req: Request, res: Response) {
+  const selfService = new SelfService();
+  const response = await selfService.selfCheck(
+    req.query.userId as string,
+    req.query.account as string
+  );
 
-    const selfService = new SelfService()
-    const response = await selfService.selfCheck(req.query.userId as string, req.query.account as string)
-
-    if (response.check) {
-        return res
-            .status(200)
-            .json(response);
-    }
-    return res
-        .status(200)
-        .json(response);
+  if (response.check) {
+    return res.status(200).json(response);
+  }
+  return res.status(200).json(response);
 }
 
 interface NationalityResponse {
-    [address: string]: string;
+  [address: string]: string;
 }
 
 export async function getNationalitiesBatch(req: Request, res: Response) {
-    try {
+  try {
+    const { addresses } = req.body;
 
-        const { addresses } = req.body;
-
-        if (!Array.isArray(addresses)) {
-            return res.status(400).json({
-                error: 'Bad request'
-            });
-        }
-
-
-        const processedAddresses = Array.from(
-            new Set(
-                addresses
-                    .filter(address => typeof address === 'string')
-                    .map(address => address.trim().toUpperCase())
-            )
-        );
-
-        if (processedAddresses.length === 0) {
-            return res.status(400).json({
-                error: 'Bad request'
-            });
-        }
-
-        console.log('Processed addresses:', processedAddresses);
-        const redisKeys = processedAddresses.map(address => `self_id:${address}`);
-
-
-        const redisResults = await Promise.all(
-            redisKeys.map(key => redisService.getCachedData(key))
-        );
-
-        const responseData: NationalityResponse = {};
-
-        processedAddresses.forEach((address, index) => {
-            const redisData = redisResults[index];
-            if (redisData && redisData.nationality) {
-                responseData[address] = redisData.nationality;
-            }
-
-        });
-
-        return res.status(200).json(responseData);
-
-    } catch (error) {
-        return res.status(500).json({
-            error: 'Internal error'
-        });
+    if (!Array.isArray(addresses)) {
+      return res.status(400).json({
+        error: 'Bad request',
+      });
     }
+
+    const processedAddresses = Array.from(
+      new Set(
+        addresses
+          .filter((address) => typeof address === 'string')
+          .map((address) => address.trim().toUpperCase())
+      )
+    );
+
+    if (processedAddresses.length === 0) {
+      return res.status(400).json({
+        error: 'Bad request',
+      });
+    }
+
+    console.log('Processed addresses:', processedAddresses);
+    const redisKeys = processedAddresses.map((address) => `self_id:${address}`);
+
+    const redisResults = await Promise.all(
+      redisKeys.map((key) => redisService.getCachedData(key))
+    );
+
+    const responseData: NationalityResponse = {};
+
+    processedAddresses.forEach((address, index) => {
+      const redisData = redisResults[index];
+      if (redisData && redisData.nationality) {
+        responseData[address] = redisData.nationality;
+      }
+    });
+
+    return res.status(200).json(responseData);
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal error',
+    });
+  }
 }
--- src/services/badges/self.service.ts
+++ src/services/badges/self.service.ts
@@ -1,70 +1,113 @@
-import { SelfBackendVerifier } from "@selfxyz/core";
-import { redisService } from "../redis.service";
-
+import {
+  AllIds,
+  AttestationId,
+  DefaultConfigStore,
+  IConfigStorage,
+  SelfBackendVerifier,
+  VerificationConfig,
+} from '@selfxyz/core';
+import { redisService } from '../redis.service';
 
 export class SelfService {
+  public async selfVerify(
+    userIdentifier: string,
+    proof: any,
+    publicSignals: string[],
+    attestationId: AttestationId,
+    userContextData: any
+  ): Promise<SelfVerifyResponse> {
+    const configStore = new DefaultConfigStore({
+      excludedCountries: [],
+    });
 
+    const selfBackendVerifier = new SelfBackendVerifier(
+      'prosperity',
+      //   'https://8e40ac4d6618.ngrok-free.app/api/self/verify',
+      'https://prosperity-passport-backend-production.up.railway.app/api/self/verify',
+      false,
+      AllIds,
+      configStore,
+      'hex'
+    );
 
-    public async selfVerify(userId: string, proof: string, publicSignals: string[]): Promise<SelfVerifyResponse> {
+    const result = await selfBackendVerifier.verify(
+      attestationId,
+      proof,
+      publicSignals,
+      userContextData
+    );
 
-        const selfBackendVerifier = new SelfBackendVerifier(
-            'prosperity',
-            'https://prosperity-passport-backend-production.up.railway.app/api/self/verify',
-            'hex',
-            false
-        );
+    if (result.isValidDetails.isValid) {
+      console.log('Verification successful:', result.isValidDetails);
 
-        const result = await selfBackendVerifier.verify(proof, publicSignals);
+      const filteredSubject = { ...result.discloseOutput };
+      const cache_key = `self_id:${userIdentifier}`;
+      redisService.setCachedData(cache_key, filteredSubject, 0);
 
-        if (result.isValid) {
-            console.log('Verification successful:', result);
-            const cache_key = `self_id_pre:${userId}`;
-            redisService.setCachedData(
-                cache_key,
-                result.credentialSubject, 0
-            );
+      //     const cache_key = `self_id_pre:${userId}`;
+      //     redisService.setCachedData(
+      //         cache_key,
+      //         result.credentialSubject, 0
+      //     );
 
-            return {
-                isValid: true,
-                credentialSubject: result.credentialSubject,
-            }
-        } else {
-            console.log('Verification failed:', result.credentialSubject);
-            return {
-                isValid: true,
-                details: result.isValidDetails,
-            }
-        }
+      //     return {
+      //         isValid: true,
+      //         credentialSubject: result.credentialSubject,
+      //     }
 
-
+      return {
+        isValid: true,
+        details: result.isValidDetails,
+        credentialSubject: filteredSubject,
+      };
+    } else {
+      console.log('Verification failed:', result.isValidDetails);
+      return {
+        isValid: false,
+        details: result.isValidDetails,
+        credentialSubject: undefined,
+      };
     }
+  }
 
-
-    public async selfCheck(userId: string, address: string): Promise<SelfCheckResponse> {
-
-        const cache_pre_key = `self_id_pre:${(userId)}`;
-        const cache_key = `self_id:${address}`;
-        const selfData = await redisService.getCachedData(cache_pre_key) ?? await redisService.getCachedData(cache_key);
-
-
-        console.log('Checking self data for userId:', userId, selfData);
-        console.log(selfData != null);
-        if (selfData != null && Object.keys(selfData).length > 0) {
-            return { message: 'Validation success!', check: true, data: selfData }
-        }
-        return { message: 'Self data not found', check: false }
+  public async selfCheck(
+    userId: string,
+    address: string
+  ): Promise<SelfCheckResponse> {
+    const selfData = await redisService.getCachedData(`self_id:${userId}`);
+    if (!selfData) {
+      return { message: 'Self data not found', check: false };
     }
+    console.log('Checking self data for userId:', userId, selfData);
+    return {
+      message: 'Validation success!',
+      check: true,
+      data: selfData,
+    };
 
+    // const cache_pre_key = `self_id_pre:${userId}`;
+    // const cache_key = `self_id:${address}`;
+    // const selfData =
+    //   (await redisService.getCachedData(cache_pre_key)) ??
+    //   (await redisService.getCachedData(cache_key));
+
+    // console.log('Checking self data for userId:', userId, selfData);
+    // console.log(selfData != null);
+    // if (selfData != null && Object.keys(selfData).length > 0) {
+    //   return { message: 'Validation success!', check: true, data: selfData };
+    // }
+    // return { message: 'Self data not found', check: false };
+  }
 }
 
 export type SelfCheckResponse = {
-    check: boolean;
-    message?: string;
-    data?: any;
-}
+  check: boolean;
+  message?: string;
+  data?: any;
+};
 
 export type SelfVerifyResponse = {
-    isValid: boolean;
-    credentialSubject?: any;
-    details?: any;
-}
+  isValid: boolean;
+  credentialSubject?: any;
+  details?: any;
+};
